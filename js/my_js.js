f244f90aabbd54e09110a9dcfeeb6a78d29cbaec
tet19 <tet19@pitt.edu>
2016-10-02 20:53:50 -0400

    updating spacing and styling

--- js/my_js.js
+++ js/my_js.js
@@ -3,7 +3,6 @@ CS 1520 - Fall 2016
 Terry Tan */
 
 function validation(event) {
-	
 	var firstName, lastName, numCats;
 	//get stuff
 	firstName = document.getElementById("firstName").value;
@@ -11,29 +10,25 @@ function validation(event) {
 	numCats = document.getElementById("numCats").value;
 	catBreeds = document.getElementsByName("catbreed");
 	var errMessage = "";
-	
-	var catcount=0;
-	for(var i=0; i<catBreeds.length; i++) {
+	var catcount = 0;
+	for (var i = 0; i < catBreeds.length; i++) {
 		if(catBreeds[i].type == "checkbox" && catBreeds[i].checked == true){
 			catcount++;
 		}
 	}
-	
-	
-	
-	if(!firstName) {
+	if ( !firstName ) {
 		errMessage += "Need an entry for first name!\n";
 	}
-	if(!lastName) {
+	if ( !lastName ) {
 		errMessage += "Need an entry for last name!\n";
 	}
-	if(isNaN(numCats) || numCats < 0 || !numCats) {
+	if (isNaN(numCats) || numCats < 0 || !numCats) {
 		errMessage += "Enter valid number for number of cats!\n";
 	}
-	if(catcount == 0) {
+	if (catcount == 0) {
 		errMessage += "Select at least one cat breed!\n";
 	}
-	if(!numCats || isNaN(numCats) || !firstName || !lastName || catcount==0) {
+	if (!numCats || isNaN(numCats) || !firstName || !lastName || catcount==0) {
 		alert("Invalid Input.\n" + errMessage);
 	}
 	else {
@@ -53,7 +48,3 @@ function hideForm(){
 function resetForm (event){
 	document.getElementById("myForm").reset();
 }
-
-
-
-	
